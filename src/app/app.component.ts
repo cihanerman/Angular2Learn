2138d18f0de9d99d86aa35b85064e09e3180dd61
cihanerman <cihan.erman@outlook.com>
2018-01-25 22:17:21 +0300

    -Proje oluşturuldu ve Vs code ile ilk commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ilk-angular',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hosgeldin Angular 2';
+}
